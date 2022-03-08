03ba827a714285f2347e145601ddaada930ed91d
Rust-CAS <jasory@rust-cas.org>
2022-03-07 18:37:02 -0700

    Create EEA.rs

---
+++ EEA.rs
@@ -0,0 +1,29 @@
+/*
+   Extended Euclidean Algorithm and Applications
+*/
+
+fn euclid_gcd(p: i64 , q: i64)->(i64,i64,i64){
+         let mut gcd : i64 =p; 
+         let mut new_r : i64 =q;
+         let mut bezout_1 : i64 =1;
+         let mut new_s : i64 =0;
+         let mut bezout_2 : i64 = 0;
+         let mut new_t: i64 = 1;
+    
+    while new_r !=0 {
+    let quotient =gcd/new_r;
+    let mut temp : i64 =new_r;
+    new_r=gcd-quotient*temp;
+    gcd=temp;
+    
+    temp=new_s;
+    new_s=bezout_1-quotient*temp;
+    bezout_1=temp;
+    
+    temp=new_t;
+    new_t=bezout_2-quotient*temp;
+    bezout_2=temp;
+    
+    }
+    (gcd,bezout_1,bezout_2)
+}
