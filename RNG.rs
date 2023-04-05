23fef1d223e4c9616dadd4965502f718b3a9be1f
Rust-CAS <jasory@rust-cas.org>
2023-04-05 00:04:37 -0700

    Update RNG.rs

--- RNG.rs
+++ RNG.rs
@@ -15,3 +15,42 @@
   }
   seed
  }
+
+
+// Xorshift 
+
+fn xor32_rng() -> u32{
+let mut x = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos() as u32;
+  x^=(x<<13);
+  x^=(x>>17);
+  x^=(x<<5);
+  x
+}
+
+// Xorshift for 64-bit, due to the low entropy provided by calling system time, the standard 64-bit Xorshift algorithm can't be used
+fn xor64_rng() -> u64{
+        let (x,y) = (xor_32(),xor_32());
+        return unsafe {std::mem::transmute::<(u32,u32),u64>((x,y))};
+}
+
+//Full PRNG function for all architectures, useful for non-cryptographic applications
+fn rng_64() -> u64 {
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {   // call RDRAND if possible
+        if is_x86_feature_detected!("rdrand"){
+            let mut x: u64 = 0;
+            unsafe { core::arch::x86_64::_rdrand64_step(&mut x) };
+            return x;
+        }
+        // use xor_64 if x86 but rdrand cannot be called
+       return xor_64()
+    }
+
+    {// use xor for all other architectures
+        xor_64()
+    }
+}
+
+
+
+
